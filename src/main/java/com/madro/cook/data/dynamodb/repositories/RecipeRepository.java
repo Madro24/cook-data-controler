01c3cf6375638e5a19d4a1c5b7880a0373de31e8
Madro24 <omadrid24@gmail.com>
2017-11-18 01:28:06 -0800

    CRUD rest controller

--- src/main/java/com/madro/cook/data/dynamodb/repositories/RecipeRepository.java
+++ src/main/java/com/madro/cook/data/dynamodb/repositories/RecipeRepository.java
@@ -1,7 +1,5 @@
 package com.madro.cook.data.dynamodb.repositories;
 
-import java.util.List;
-
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,5 +8,5 @@ import com.madro.cook.models.Recipe;
 @EnableScan
 public interface RecipeRepository extends CrudRepository<Recipe, String> {
 
-	List<Recipe> findById(String id);
+	Recipe findByName(String name);
 }
--- src/main/java/com/madro/cook/rest/RecipeRestController.java
+++ src/main/java/com/madro/cook/rest/RecipeRestController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.madro.cook.data.dynamodb.repositories.RecipeRepository;
@@ -20,8 +22,20 @@ public class RecipeRestController {
 		return (List<Recipe>) recipeRepository.findAll();
 	}
 	
-	@RequestMapping("/recipes/{id}")
-	public List<Recipe> getRecipeById(@PathVariable(value = "id") String id) {
-		return recipeRepository.findById(id);
+	@RequestMapping("/recipes/{name}")
+	public Recipe getRecipe(@PathVariable(value = "name") String name) {
+		return recipeRepository.findByName(name);
+	}
+	
+	@RequestMapping(method= {RequestMethod.POST, RequestMethod.PUT}, value="/recipes")
+	public List<Recipe> addRecipe(@RequestBody Recipe recipe) {
+		recipeRepository.save(recipe);
+		return getRecipes();
+	}
+	
+	@RequestMapping(method= RequestMethod.DELETE, value="/recipes/{name}")
+	public List<Recipe> removeRecipe(@PathVariable(value = "name") String name) {
+		recipeRepository.delete(name);
+		return getRecipes();
 	}
 }
